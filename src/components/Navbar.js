e92ff2dd8b8bd01d186d9c1f36f274f06f673b2f
nash <24046195@myrp.edu.sg>
2026-01-23 22:54:31 +0800

    Home.js + Navbar.js editted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,40 @@
+// import { NavLink } from "react-router-dom";
+
+
+// export default function Navbar() {
+//   return (
+//     <header>
+//       <strong>Card App</strong>
+//       <nav 
+//         style={{ backgroundColor: "black", padding: "10px 20px"}}>
+//       <div
+//         style={{
+//           display: "flex",
+//           gap: "20px"}}>
+//           <NavLink to="/"> Home </NavLink>
+//           <NavLink to="/cards"> Cards </NavLink>
+//           <NavLink to="/cards/new"> Add Card </NavLink>
+//         </div>
+//       </nav>
+//     </header>
+//   );
+// }
+
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <header>
-      <strong>Card App</strong>
-      <nav>
-        <NavLink to="/" end>
-          Home
-        </NavLink>
-        <NavLink to="/cards">
-          Cards
-        </NavLink>
-        <NavLink to="/cards/new">
-          Add Card
-        </NavLink>
+      <nav
+        style={{backgroundColor: "black", padding: "12px 24px" }}>
+        <div style={{ display: "flex", alignItems: "center", gap: "35px" }}>
+
+          <strong style={{ color: "Yellow" }}>Card App</strong>
+
+          <NavLink to="/" style={{ color: "pink",}}> Home </NavLink>
+          <NavLink to="/cards" style={{ color: "pink",}}> Cards </NavLink>
+          <NavLink to="/cards/new" style={{ color: "pink",}}> Add Card </NavLink>
+        </div>
       </nav>
     </header>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,56 @@
 import { Link } from "react-router-dom";
 
+// export default function Home() {
+//   /* TODO: Design and complete the Home page
+//     - display instructions
+//     - link to Cards page
+//     - style as a landing page */
+
+//   return (
+//     <main>
+//       <h1>Welcome to Card App</h1>
+//       <p>Manage your card collection here.</p>
+//       <Link to="/cards">View Cards</Link>
+//     </main>
+//   );
+// }
+
+import Img from "../assets/image.png";
+
 export default function Home() {
-  /* TODO: Design and complete the Home page
-    - display instructions
-    - link to Cards page
-    - style as a landing page */
   return (
-    <main>
-      <h1>Welcome to Card App</h1>
-      <p>Manage your card collection here.</p>
-      <Link to="/cards">View Cards</Link>
-    </main>
+    <div
+      className="container"
+      style={{
+        backgroundColor: "#231a1a",
+        color: "white"
+      }}
+    >
+      <img
+        className="img-feat"
+        src={Img}
+        alt="magic the gathering"
+
+        style={{
+          width: "900px",
+          height: "auto",
+          display: "block",
+          margin: "0 auto"
+        }} />
+
+      <main>
+        <h1>Welcome to the Magic the Gathering Card Web!</h1>
+        <h5>In the Magic™ game, you are counted among the elite spellcasters of the Multiverse—the Planeswalkers. Some are
+          friends and others are foes. All wield terrifying magic and command armies of creatures torn from the endless planes of
+          reality. Your deck of cards represents all the weapons in your arsenal. It contains the spells you know and the creatures
+          you can summon to fight for you. Customize your own Magic deck, then challenge your friends to see whose spark
+          burns the brightest!</h5>
+        <p>Manage your card collection here</p>
+        <Link to="/cards">
+          <button type="button" style={{ backgroundColor: "pink", color: "black" }}>View Cards</button>
+        </Link>
+
+      </main>
+    </div>
   );
 }
